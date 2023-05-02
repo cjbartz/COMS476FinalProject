d636b9b174867aa33840b99adb91a53ff069f622
Carver Bartz <cjbartz@iastate.edu>
2023-05-01 21:01:30 -0500

    small changes

--- restaurant_robot_planning/lib/main.dart
+++ restaurant_robot_planning/lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'tree.dart';
 
 void main() {
+  //test_bfs();
+  test_RRT();
   runApp(MyApp());
+}
+
+void test_bfs(){
+  Node n1 = Node(x: 10, y: 10);
+  Node n2 = Node(x: 11, y: 11);
+  Node n3 = Node(x: 12, y: 12);
+  Node n4 = Node(x: 13, y: 13);
+
+  Edge e1 = Edge(node1: n1, node2: n2);
+  Edge e2 = Edge(node1: n2, node2: n3);
+  Edge e3 = Edge(node1: n2, node2: n4);
+  Edge e4 = Edge(node1: n4, node2: n1);
+
+  Tree myTree = Tree();
+  myTree.add_node(n1);
+  myTree.add_node(n2);
+  myTree.add_node(n3);
+  myTree.add_node(n4);
+
+  myTree.add_edge(e1);
+  myTree.add_edge(e2);
+  myTree.add_edge(e3);
+  myTree.add_edge(e4);
+
+  List<Node> test = myTree.bfs(n1, n4);
+  for(var node in test){
+    node.print_node();
+  }
+}
+
+void test_RRT(){
+  Node n1 = Node(x: 13.9, y: 17.9);
+  Node n2 = Node(x: 76.2, y: 40.9);
+  Tree myTree = Tree();
+  myTree.generateRRT(n1, n2, [], 100, 0.1);
+  List<Node> test = myTree.bfs(n1, n2);
+  for(var node in test){
+    node.print_node();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -25,7 +67,7 @@ class MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('CustomPaint example'),
+        title: Text('Restaurant Robot App'),
       ),
       body: GestureDetector(
         onTapDown: (details) {
@@ -60,6 +102,16 @@ class MyHomePageState extends State<MyHomePage> {
                     shape: BoxShape.circle,
                   ),
                 ),
+              ),
+              Positioned(
+                bottom: 50, // Adjust the horizontal position
+                right: 50, // Adjust the vertical position
+                child: ElevatedButton(
+                  child: Text("Confirm"),
+                  onPressed: () {
+                    //TODO
+                  },
+                )
               ),
               // User drawn circles
             ]..addAll(
--- restaurant_robot_planning/lib/tree.dart
+++ restaurant_robot_planning/lib/tree.dart
@@ -1,11 +1,150 @@
 
 
+import 'dart:collection';
+import 'dart:math';
+import 'dart:ui';
+
+import 'obstacles.dart';
+
 class Tree {
   List<Node> nodes = [];
   List<Edge> edges = [];
 
   // Constructor
   Tree();
+
+  void add_node(Node new_node){
+    nodes.add(new_node);
+  }
+
+  void add_edge(Edge new_edge){
+    edges.add(new_edge);
+  }
+
+  List<Node> bfs(Node startNode, Node targetNode) {
+    Map<Node, Node> visited = {}; // Keeps track of visited nodes and their parent
+    Queue<Node> queue = Queue<Node>();
+
+    visited[startNode] = startNode;
+    queue.add(startNode);
+
+    while (queue.isNotEmpty) {
+      Node currentNode = queue.removeFirst();
+
+      if (currentNode == targetNode) {
+        return _buildPath(visited, currentNode);
+      }
+
+      for (Edge edge in edges) {
+        if (edge.node1 == currentNode) {
+          if (!visited.containsKey(edge.node2)) {
+            visited[edge.node2] = currentNode;
+            queue.add(edge.node2);
+          }
+        } else if (edge.node2 == currentNode) {
+          if (!visited.containsKey(edge.node1)) {
+            visited[edge.node1] = currentNode;
+            queue.add(edge.node1);
+          }
+        }
+      }
+    }
+
+    return []; // Return an empty list if no path is found
+  }
+
+  List<Node> _buildPath(Map<Node, Node> visited, Node currentNode) {
+    List<Node> path = [];
+    Node? node = currentNode;
+
+    while (node != visited[node]) {
+      path.add(node!);
+      node = visited[node];
+    }
+
+    if (node != null) {
+      path.add(node); // Add the start node
+    }
+    return path.reversed.toList();
+  }
+
+  void generateRRT(Node start, Node goal, List<Obstacle> obstacles, int maxNodes, double maxStepSize) {
+    Random random = Random();
+
+    nodes.add(start);
+
+    for (int i = 0; i < maxNodes; i++) {
+      // Generate a random point in the space
+      double randomX = random.nextDouble() * (goal.x - start.x) + start.x;
+      double randomY = random.nextDouble() * (goal.y - start.y) + start.y;
+      Node randomPoint = Node(x: randomX, y: randomY);
+
+      // Find the nearest point in the tree, considering both nodes and points on edges
+      Node nearestPoint = start;
+      double minDistance = _distance(nearestPoint, randomPoint);
+
+      for (Edge edge in edges) {
+        Node edgePoint = _nearestPointOnEdge(edge, randomPoint);
+        double currentDistance = _distance(edgePoint, randomPoint);
+        if (currentDistance < minDistance) {
+          nearestPoint = edgePoint;
+          minDistance = currentDistance;
+        }
+      }
+
+      // Calculate the point in the direction of the random point, limited by maxStepSize
+      double theta = atan2(randomPoint.y - nearestPoint.y, randomPoint.x - nearestPoint.x);
+      double newX = nearestPoint.x + cos(theta) * min(maxStepSize, minDistance);
+      double newY = nearestPoint.y + sin(theta) * min(maxStepSize, minDistance);
+      Node newPoint = Node(x: newX, y: newY);
+
+      // Check for collisions
+      Edge newEdge = Edge(node1: nearestPoint, node2: newPoint);
+      bool collision = false;
+      for (Obstacle obstacle in obstacles) {
+        if (obstacle.isInCollision(newEdge)) {
+          collision = true;
+          break;
+        }
+      }
+
+      // If no collision, add the new point and edge to the tree
+      if (!collision) {
+        nodes.add(newPoint);
+        edges.add(newEdge);
+      }
+    }
+  }
+
+  Node _nearestPointOnEdge(Edge edge, Node point) {
+    Node a = edge.node1;
+    Node b = edge.node2;
+
+    double vx = b.x - a.x;
+    double vy = b.y - a.y;
+    double px = point.x - a.x;
+    double py = point.y - a.y;
+
+    double edgeLengthSquared = vx * vx + vy * vy;
+    double t = (px * vx + py * vy) / edgeLengthSquared;
+
+    if (t < 0) t = 0;
+    if (t > 1) t = 1;
+
+    double nearestX = a.x + t * vx;
+    double nearestY = a.y + t * vy;
+
+    return Node(x: nearestX, y: nearestY);
+  }
+
+
+  // Using Eucliden Distance
+  double _distance(Node a, Node b) {
+    double dx = a.x - b.x;
+    double dy = a.y - b.y;
+    return sqrt(dx * dx + dy * dy);
+  }
+
 }
 
 class Node {
@@ -15,6 +154,14 @@ class Node {
   // Constructor
   Node({required this.x, required this.y});
 
+  // Using this constructor example:
+  // Offset myOffset = Offset(10.0, 20.0);
+  // Node myNode = Node.fromOffset(myOffset);
+
+  factory Node.fromOffset(Offset offset) {
+    return Node(x: offset.dx, y: offset.dy);
+  }
+
   // Getters
   double get getX => x;
   double get getY => y;
@@ -22,6 +169,10 @@ class Node {
   // Setters
   set setX(double newX) => x = newX;
   set setY(double newY) => y = newY;
+
+  void print_node(){
+    print('' + x.toString() + ', ' + y.toString());
+  }
 }
 
 class Edge {
